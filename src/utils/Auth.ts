156c7770a4fcd1c5ba28418b3f0800a5271ac777
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2024-01-27 15:47:45 -0300

    auth

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -1,16 +1,29 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from '@firebase/auth'
-import User from '../models/User'
-import { auth } from '../database/Firestore'
+import { GoogleAuthProvider, OAuthCredential, signInWithPopup, UserCredential, signInWithCustomToken, getAuth } from '@firebase/auth'
+import { User } from '../models/Users'
+import firesotoreDatabase from '@/database';
 
 
-export async function LoginWithPopup(): Promise<User> {
-    const provider = await new GoogleAuthProvider();
-    const { user } = await signInWithPopup(auth, provider); 
-    const userInfo : User = {
-        google_id: user.uid,
-        name: user.displayName,
-        photoURL: user.photoURL 
-    } 
-    return userInfo;
+export async function LoginWithPopup(): Promise<User | null> {
+    try {
+        const provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(firesotoreDatabase._auth, provider); 
+        const user = result.user;
+        const userInfo : User = {
+            google_id: user.uid,
+            name: user.displayName,
+            photoURL: user.photoURL,
+            email: user.email!,
+        }
+        const alreadyLoggedIn = await firesotoreDatabase.GetUserByEmail(userInfo.email)
+        console.log('already logged in: ', alreadyLoggedIn);
+        
+        if(!alreadyLoggedIn) await firesotoreDatabase.AddUser(userInfo);
+        
+        return userInfo;
+    } catch (e) {
+        console.log('login canceled');
+        return null;
+    }
+        
 }
 
