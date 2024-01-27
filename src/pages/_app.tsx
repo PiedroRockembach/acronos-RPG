70d3cc14c5ddad6c4a4dd92285a9108d09339e80
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2024-01-27 15:48:59 -0300

    shadcn config and imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from 'next/app'
 
+import '../styles/base.css'
+import '../styles/output.css'
+import '../styles/lobby.css'
+import '../styles/header.css'
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
